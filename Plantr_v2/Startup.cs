17e94ff50b5606bbdd5a22a123f0d59360b27d18
austincrisp <austin.crisp05@gmail.com>
2017-03-16 22:56:39 -0400

    Add project files.

---
+++ Plantr_v2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Plantr_v2.Startup))]
+namespace Plantr_v2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
